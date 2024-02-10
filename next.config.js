6439d01f1d6ada75f0b0344c33c4c3e8ed590e11
sashascode <racagnisasha@gmail.com>
2024-02-10 14:51:48 -0300

    Add .env to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org'],
   },
+  env: {
+    MOVIE_API_KEY: process.env.MOVIE_API_KEY,
+    GET_MOVIE_DETAILS_URL: process.env.GET_MOVIE_DETAILS_URL,
+    SEARCH_MOVIE_URL: process.env.SEARCH_MOVIE_URL,
+  },
   reactStrictMode: true,
   productionBrowserSourceMaps: false,
   webpack: (config, { isServer }) => {
